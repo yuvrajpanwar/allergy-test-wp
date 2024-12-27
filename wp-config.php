906997c5fb943d3eccddb93160ef500326ac47c4
yuvrajpanwar <yogipanwar173@gmail.com>
2024-12-27 12:01:24 +0530

    final 2

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhos' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
